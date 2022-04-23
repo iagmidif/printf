a588100f947b22896e97a6e658bfb9e187769a7b
iagmidif <tariq86hamid@gmail.com>
2022-04-23 16:06:00 +0300

    update

--- print_num.c
+++ print_num.c
@@ -15,19 +15,16 @@ int print_int(va_list params)
 
 	n = va_arg(params, int);
 	count = 0;
-	if (n < 0)
-	{
-		count += _putchar('-');
-		n *= -1;
-	}
-
 	len = snprintf(NULL, 0, "%d", n);
 	str = malloc(len + 1);
+
 	snprintf(str, len + 1, "%d", n);
 	while (*str)
 	{
 		count += _putchar(*str);
 		str++;
 	}
+	str -= count;
+	free(str);
 	return (count);
 }
