48881ff3702a6a56168a987db4e57ad8b0be9043
iagmidif <tariq86hamid@gmail.com>
2022-04-23 15:16:13 +0300

    print_char and print_str

---
+++ print_alpha.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_char - prints a char type
+ * @params: va_list arguments
+ *
+ * Return: 1 if successful
+ * 0 otherwise
+ */
+int print_char(va_list params)
+{
+	return(_putchar(va_arg(params, int)));
+}
+
+/**
+ * print_str - prints a string
+ * @params: va_list arguments
+ *
+ * Return: number of characters printed
+ */
+int print_str(va_list params)
+{
+	int count = 0;
+	char *str = va_arg(params, char *);
+
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
+	return (count);
+}
