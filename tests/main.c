bc35cd35eaa4e961c724995e85882e925cd37fba
iagmidif <tariq86hamid@gmail.com>
2022-04-23 15:48:48 +0300

    added tests for print_int

--- tests/main.c
+++ tests/main.c
@@ -11,7 +11,9 @@ int main(void)
 {
 	int len = _printf("Let's try to printf a simple sentence.\n");
 	int len2 = printf("Let's try to printf a simple sentence.\n");
-	printf("%d %d\n", len, len2);
+	
+	_printf("%d %i\n", len, len2);
+	printf("%d %i\n", len, len2);
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
@@ -24,5 +26,13 @@ int main(void)
 	printf("%s%c\n");
 	_printf("");
 	printf("");
+	len = _printf("Percent:[%%]\n");
+    	len2 = printf("Percent:[%%]\n");
+    	_printf("Len:[%d]\n", len);
+    	printf("Len:[%d]\n", len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+
+
 	return (0);
 }
